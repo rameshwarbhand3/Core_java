2939d926bd034430f1ba79393feec7aae6bfb9a4
rameshwarbhand3 <rameshwarbhand3@gmail.com>
2023-02-26 21:20:34 +0530

    Array Demo programs

---
+++ java-T-point/src/com/ram/array/TestArray.java
@@ -0,0 +1,10 @@
+package com.ram.array;
+
+class TestArray {
+    public static void main(String[] args) {
+        int arr[] = {3, 4, 5, 6};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
