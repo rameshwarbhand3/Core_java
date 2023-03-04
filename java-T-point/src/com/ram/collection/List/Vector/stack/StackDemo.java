1e13e9468e2231e27d79b7bd7b7f405f693c8d60
rameshwarbhand3 <rameshwarbhand3@gmail.com>
2023-03-04 08:40:24 +0530

    collection framework basic example.

---
+++ java-T-point/src/com/ram/collection/List/Vector/stack/StackDemo.java
@@ -0,0 +1,21 @@
+package com.ram.collection.List.Vector.stack;
+
+import java.util.*;
+//Stack is subclass of vector class.
+public class StackDemo {
+    public static void main(String args[]) {
+        Stack<String> st = new Stack<String>();
+        st.push("Ashish");
+        st.push("Garima");
+        st.push("Vishal");
+        st.push("Sparsh");
+       
+        System.out.println(st.peek());
+        st.pop();
+        System.out.println(st);
+        Iterator<String> itr = st.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+}
