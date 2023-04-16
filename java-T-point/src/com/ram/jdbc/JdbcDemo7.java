936c1f579a8a89b92df80bd7854b37695edb7f46
rameshwarbhand3 <rameshwarbhand3@gmail.com>
2023-04-16 22:32:51 +0530

    jdbc example work in progress

--- java-T-point/src/com/ram/jdbc/JdbcDemo7.java
+++ java-T-point/src/com/ram/jdbc/JdbcDemo7.java
@@ -1,31 +1,30 @@
 package com.ram.jdbc;
-import java.sql.*;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class jdbcDemo7 {
-    public static void main(String args[]) throws Exception{
+public class JdbcDemo7 {
+    public static void main(String args[]) throws Exception {
         String driver = "com.mysql.cj.jdbc.Driver";
         String jdbc_urls = "jdbc:mysql://localhost:3306/emp_db";
         String user = "root";
         String pwd = "root";
         Class.forName(driver);
-        Connection con = DriverManager.getConnection(jdbc_urls,user,pwd);
+        Connection con = DriverManager.getConnection(jdbc_urls, user, pwd);
         Statement st = con.createStatement();
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number : ");
         int num = sc.nextInt();
-        String sql_query = "SELECT * FROM(select id,first_name,salary,rank() over (order by salary DESC) ranking from Employee)
-                where ranking="+num;
-        ResultSet rs = st.executeQuery(sql_query);
+        String query = "SELECT * FROM(select id,first_name,salary,rank() over (order by salary DESC) ranking from Employee)as emp " +
+                "where ranking=" + num;
+        ResultSet rs = st.executeQuery(query);
 
-        while (rs.next()){
-            System.out.println(rs.getInt(1)+"\t"+rs.getString(2)+"\t"+rs.getInt(3));
+        while (rs.next()) {
+            System.out.println(rs.getInt(1) + "\t" + rs.getString(2) + "\t" + rs.getInt(3));
         }
         con.close();
-    }
     }
 }
